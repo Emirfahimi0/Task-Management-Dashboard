a74e48efa9b878af24900955598c090416ecf492
Emirfahimi0 <emirfahimi0@gmail.com>
2024-01-09 10:51:12 +0800

    add copywriting

---
+++ task-x/src/constant/english.ts
@@ -0,0 +1,23 @@
+export const ENGLISH = {
+  BUTTON: {
+    DELETE: "Delete",
+    COMPLETE: "Complete",
+    INCOMPLETE: "Incomplete",
+    EDIT: "Edit",
+    ADD_TASK: "Add Task",
+  },
+  LABEL: {
+    TITLE_HEADER: "Task Management Dashboard",
+    TASK_ADDED_LABEL: "New Task Added!",
+  },
+
+  TASK: {
+    ERROR_TITLE_BLANK: "Title cannot be empty",
+    ERROR_TITLE_LENGTH: "Title length cannot be less than 10 characters",
+    DUE_DATE_LABEL: " Due Date: ",
+  },
+  INPUT: {
+    PLACE_HOLDER: "Create a new task",
+    ERROR_SNAP: "Oh Snapp!",
+  },
+};
