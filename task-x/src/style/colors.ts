042c451e87a024b45628852559eb56d15b8f662c
Emirfahimi0 <emirfahimi0@gmail.com>
2024-01-09 10:54:06 +0800

    add styles

---
+++ task-x/src/style/colors.ts
@@ -0,0 +1,57 @@
+import { ColorStyle } from "../@types/colorPalletes";
+
+export const colorGreen = {
+  100: "#dcfce7",
+  400: "#4ade80",
+  500: "#22c55e",
+  600: "#34d399",
+};
+
+export const colorBlue = {
+  100: "#cffafe",
+  300: "#7dd3fc",
+  400: "#38bdf8",
+  900: "#172554",
+};
+
+export const colorPurple = {
+  400: "#a78bfa",
+  500: "#d946ef",
+  700: "#7e22ce",
+  900: "#4c1d95",
+};
+
+export const colorPink = {
+  100: "#fce7f3",
+  500: "#ec4899",
+  600: "#db2777",
+  800: "#9d174d",
+};
+
+export const colorRed = {
+  200: "#fecaca",
+  300: "#fca5a5",
+  500: "#ef4444",
+  600: "#e11d48",
+};
+
+export const colorGray = {
+  100: "#f4f4f5",
+  400: "#9ca3af",
+  600: "#4b5563",
+  700: "#334155",
+};
+
+export const colorStyle: ColorStyle = {
+  blue: colorBlue,
+  purple: colorPurple,
+  pink: colorPink,
+  orange: "#ff7849",
+  green: colorGreen,
+  yellow: "#ffc82c",
+  white: "#ffffff",
+  red: colorRed,
+  "gray-dark": "#374151",
+  gray: colorGray,
+  "gray-light": "#d1d5db",
+};
---
+++ task-x/src/style/common.ts
@@ -0,0 +1,16 @@
+export const centerHW = "flex flex-col  justify-center item-center " as const;
+
+export const centerText = "text-center" as const;
+
+export const centerHorizontal = "flex justify-center item-center" as const;
+
+export const flexRow = "flex flex-row" as const;
+export const flexCol = "flex flex-col" as const;
+
+export const fullWidth = "w-full" as const;
+export const fullHeight = "h-full" as const;
+
+export const textBase = "text-base" as const;
+export const textSmall = "text-sm" as const;
+export const textLarge = "text-lg" as const;
+export const textXL = "text-xl" as const;
