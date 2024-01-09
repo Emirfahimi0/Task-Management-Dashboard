2dd124c3be493fb121f5899afe9940c783b604e2
Emirfahimi0 <emirfahimi0@gmail.com>
2024-01-09 10:49:50 +0800

    configure tailwind json

---
+++ task-x/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import {colorStyle} from "./src/style"
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],  theme: {
+    colors:colorStyle,
+    extend: {
+      fontFamily:{
+        'sans':['Work Sans', "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
+
