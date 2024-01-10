7bd96c8ff7138074826094b7f959d187b0881a8d
Emirfahimi0 <emirfahimi0@gmail.com>
2024-01-10 14:40:25 +0800

    update configure project

--- task-x/tailwind.config.js
+++ task-x/tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import {colorStyle} from "./src/style"
+import { colorStyle } from "./src/style";
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
-    colors:colorStyle,
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: colorStyle,
     extend: {
-      fontFamily:{
-        'sans':['Work Sans', "sans-serif"]
-      }
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
-
+};
