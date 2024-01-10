19a1d22b8af0493df82abef9e7e8361a29debd93
Emirfahimi0 <emirfahimi0@gmail.com>
2024-01-10 14:39:24 +0800

    update constant

--- task-x/src/constant/english.ts
+++ task-x/src/constant/english.ts
@@ -9,12 +9,18 @@ export const ENGLISH = {
   LABEL: {
     TITLE_HEADER: "Task Management Dashboard",
     TASK_ADDED_LABEL: "New Task Added!",
+    TASK_SUB_LABEL: "Make sure to finish your task accordingly!",
+    RECENT_TASK_LABEL: "Recently Task",
+    COMPLETED_TASK_LABEL: "Completed Task",
+    IMPORTANT_TASK_LABEL: "Important Task",
   },
 
   TASK: {
     ERROR_TITLE_BLANK: "Title cannot be empty",
     ERROR_TITLE_LENGTH: "Title length cannot be less than 10 characters",
     DUE_DATE_LABEL: " Due Date: ",
+    MARK_COMPLETE_LABEL: "Complete",
+    MARK_INCOMPLETE_LABEL: "Incomplete",
   },
   INPUT: {
     PLACE_HOLDER: "Create a new task",
